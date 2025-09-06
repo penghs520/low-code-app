4ce21df7e5b3c8df51a4def639a74940d970fcf2
penghs <penghs@agilean.cn>
2025-09-06 21:58:46 +0800

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+      prettierConfig, // 禁用与 Prettier 冲突的 ESLint 规则
+    ],
+    plugins: {
+      prettier,
+    },
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      "prettier/prettier": "error", // 将 Prettier 格式化错误作为 ESLint 错误显示
+    },
+  },
+]);
